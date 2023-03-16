c444e252529d84c7a16c0948e90442f1e7e10ec8
Mallidi Akhil Reddy <malakhil@WKMZTF0C33DC.global.publicisgroupe.net>
2023-03-16 12:45:01 +0530

    feat: added implementation to fetch products and product data from server

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import ProductsList from '../../components/ProductsList/ProductsList';
 
 import './Home.scss';
 
-import products from './../../products';
-
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    }
+    fetchProducts();
+  }, []);
+
+  console.log('=> products', products);
+
   return (
     <div className='home'>
       <h2> Latest Products </h2>
--- client/src/pages/Pdp/Pdp.js
+++ client/src/pages/Pdp/Pdp.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
-
-import products from './../../products';
 
 import './Pdp.scss';
 
 const Pdp = () => {
+  const [product, setProduct] = useState({});
   const { id } = useParams();
-  const product = products.find(p => p._id === id);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [ id ]);
 
   return (
     <div className='pdp'>  
